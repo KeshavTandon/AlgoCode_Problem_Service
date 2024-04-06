615697d67c7f0d4d4fe868ca4ace495397cf8558
KeshavTandon <keshavtandon51@gmail.com>
2024-04-06 20:22:41 +0530

    Feat:Added unimplemented error for api contract

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,21 +1,41 @@
-function pingCheckController(req,res){
-    return res.json({message:"Ping controller is up"})
+function pingCheckController(req, res) {
+  return res.json({ message: "Ping controller is up" });
 }
-function addProblem(req, res) {}
-
-function getProblem(req, res) {}
-
-function getProblems(req, res) {}
-
-function deleteProblem(req, res) {}
-
-function updateProblem(req, res) {}
-
-module.exports={
-    addProblem,
-    getProblem,
-    getProblems,
-    deleteProblem,
-    updateProblem,
-    pingCheckController
+function addProblem(req, res) {
+  return res.status(501).json({
+    message:'Not Implemented'
+  });
 }
+
+function getProblem(req, res) {
+    return res.status(501).json({
+      message: "Not Implemented",
+    });
+}
+
+function getProblems(req, res) {
+    return res.status(501).json({
+      message: "Not Implemented",
+    });
+}
+
+function deleteProblem(req, res) {
+    return res.status(501).json({
+      message: "Not Implemented",
+    });
+}
+
+function updateProblem(req, res) {
+    return res.status(501).json({
+      message: "Not Implemented",
+    });
+}
+
+module.exports = {
+  addProblem,
+  getProblem,
+  getProblems,
+  deleteProblem,
+  updateProblem,
+  pingCheckController,
+};
